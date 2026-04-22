0e49a0e1bb211520996a5c9c891599281550928f
ptpe31 <ptpe31@gmail.com>
2026-04-22 02:17:39 +0200

    fix: prevent release startup crash (native deps + bootstrap)

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
+import '../src/api/firebaseIndexedDbGuard';
 import 'react-native-gesture-handler';
+import 'react-native-worklets';
+import 'react-native-reanimated';
+import '../src/services/performance';
+import '../src/locales/i18n';
+import '../src/services/backgroundTasks';
+
+import * as SplashScreen from 'expo-splash-screen';
 import { registerRootComponent } from 'expo';
+
+void SplashScreen.preventAutoHideAsync().catch(() => {});
+
 import App from './App';
 
 registerRootComponent(App);
